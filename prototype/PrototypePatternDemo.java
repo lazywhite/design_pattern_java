e0b0bb5e61e760818f8a563310c92adff0dda164
white <346816483@qq.com>
2016-08-26 19:22:10 +0800

    add note for Prototype

---
+++ prototype/PrototypePatternDemo.java
@@ -0,0 +1,18 @@
+public class PrototypePatternDemo {
+    public static void main(String[] args) {
+        try{
+        ShapeCache.loadCache();
+
+        Shape clonedShape = (Shape) ShapeCache.getShape("circle");
+        System.out.println("Shape: " + clonedShape.getType());
+
+        Shape clonedShape2 = (Shape) ShapeCache.getShape("square");
+        System.out.println("Shape: " + clonedShape2.getType());
+        
+        Shape clonedShape3 = (Shape) ShapeCache.getShape("rectangle");
+        System.out.println("Shape: " + clonedShape3.getType());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
