9a467555bbd634adc1d20f45a96c75d40fd4abbc
white <346816483@qq.com>
2016-08-27 17:37:07 +0800

    add memento pattern

---
+++ memento/Memento.java
@@ -0,0 +1,10 @@
+public class Memento {
+    private String state;
+    public Memento(String state){
+        this.state = state;
+    }
+
+    public String getState(){
+        return state;
+    }
+}
