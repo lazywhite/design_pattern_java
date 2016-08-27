a33d35290de49373d58ebce791ecbcdd30422ffd
white <346816483@qq.com>
2016-08-27 15:52:21 +0800

    add chain pattern

---
+++ chain/ChainPatternDemo.java
@@ -0,0 +1,21 @@
+public class ChainPatternDemo{
+    public static AbstractLogger getChainOfLoggers(){
+        AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+
+
+        errorLogger.setNextLogger(fileLogger);
+        fileLogger.setNextLogger(consoleLogger);
+
+        return errorLogger;
+    }
+    public static void main(String[] args){
+        AbstractLogger loggerChain = getChainOfLoggers();
+        loggerChain.logMessage(AbstractLogger.ERROR, "Error level message");
+        System.out.println();
+        loggerChain.logMessage(AbstractLogger.DEBUG, "Debug level message");
+        System.out.println();
+        loggerChain.logMessage(AbstractLogger.INFO, "Info level message");
+    }
+}
