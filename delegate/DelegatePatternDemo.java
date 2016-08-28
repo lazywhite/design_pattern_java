7c4d5dc74c3b4c65dfb967ba5956e4da1b017af4
white <346816483@qq.com>
2016-08-28 14:47:20 +0800

    add delegate pattern

---
+++ delegate/DelegatePatternDemo.java
@@ -0,0 +1,15 @@
+/** 
+ * Type: structural
+ */
+public class DelegatePatternDemo {
+    public static void main(String[] args){
+        BusinessDelegate businessDelegate = new BusinessDelegate();
+        businessDelegate.setServiceType("EJB");
+
+        Client client = new Client(businessDelegate);
+        client.doTask();
+
+        businessDelegate.setServiceType("JMS");
+        client.doTask();
+    }
+}
