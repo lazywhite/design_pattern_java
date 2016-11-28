727b9eeee018714bf0c77dfa4030083ef5fa6ab2
white <346816483@qq.com>
2016-11-28 18:41:57 +0800

    update singleton pattern

--- singleton/SingletonPatternDemo.java
+++ singleton/SingletonPatternDemo.java
@@ -1,5 +1,7 @@
 /**
  * PatterType: Creation
+ * 懒汉模式： 类加载时不初始化, 类加载时才初始化， 加载快但访问类的唯一实例慢
+ * 饿汉模式： 类加载时就完成了初始化， 类加载较慢但获取对象的速度快
 **/
 public class SingletonPatternDemo {
     public static void main(String[] args){
